2f1e1c296ba2f1ec107fda8cc8164395eb8ec77d
Gregor MacDougall <gregor@freerunningtechnologies.com>
2013-01-07 15:30:26 -0800

    Fixed a broken migration.
    This migration would not work properly, as the calculable would not be
    saved until the shipping method was created.
    
    Change-Id: I81d66c449435391b4c133e7e99fab361b5ea18bc

--- db/migrate/20121222000413_add_email_shipping_method.rb
+++ db/migrate/20121222000413_add_email_shipping_method.rb
@@ -5,7 +5,7 @@ class AddEmailShippingMethod < ActiveRecord::Migration
       shipping_method = Spree::ShippingMethod.new :name => Spree::ShippingMethod::ELECTRONIC_DELIVERY_NAME
       shipping_method.display_on = :back_end
       shipping_method.zone = zone
-      shipping_method.calculator = Spree::Calculator::FlatPercentItemTotal.create!
+      shipping_method.calculator = Spree::Calculator::FlatPercentItemTotal.new
       shipping_method.save!
     end
   end
