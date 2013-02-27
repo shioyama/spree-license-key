fd2ceddb050374d2fb1e5a6e032a38d18aa948eb
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2013-02-26 23:23:33 -0800

    Variant has many license keys
    Change-Id: I4f5a4a7acee22afe6ee10404fdc9db94e6a8ee1d

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,3 +1,4 @@
 Spree::Variant.class_eval do
+  has_many :license_keys, class_name: 'Spree::LicenseKey'
   has_and_belongs_to_many :license_key_types, :join_table => :spree_variant_license_key_types, :class_name => "Spree::LicenseKeyType"
 end
