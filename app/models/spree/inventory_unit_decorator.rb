69d7d955fa49b13e7adeffe555debae25fe2a78c
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2013-03-05 23:28:04 -0800

    nullify license key if inventory unit is deleted
    Change-Id: I6e89e2b36b186538939a6c44b2bb03259f712a80

--- app/models/spree/inventory_unit_decorator.rb
+++ app/models/spree/inventory_unit_decorator.rb
@@ -2,7 +2,7 @@ Spree::InventoryUnit.class_eval do
   scope :electronically_delivered, joins(:variant).where('spree_variants.electronic_delivery_keys IS NOT NULL').readonly(false)
   scope :physically_delivered, joins(:variant).where('spree_variants.electronic_delivery_keys IS NULL').readonly(false)
 
-  has_many :license_keys, class_name: "Spree::LicenseKey"
+  has_many :license_keys, class_name: "Spree::LicenseKey", dependent: :nullify
 
   delegate :electronic_delivery_keys, :to => :variant
 end
