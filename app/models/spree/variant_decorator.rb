bdffb9bdfade2560f69b71ad61407a33e4bd91ee
Richard Wilson <richard@freerunningtech.com>
2013-12-10 11:09:26 -0800

    Attr accessible electronic_delivery information
    Change-Id: I1a14a6186370c7c196433af1595a573fb5de136a

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,6 +2,8 @@ Spree::Variant.class_eval do
   has_many :license_keys, class_name: 'Spree::LicenseKey'
   has_and_belongs_to_many :license_key_types, :join_table => :spree_variant_license_key_types, :class_name => "Spree::LicenseKeyType"
 
+  attr_accessible :electronic_delivery_keys, :electronic_delivery
+
   validate :electronic_delivery_set
 
   private
