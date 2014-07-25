e6f691af8b1271cddb2e659b5cff65ee1d3dcb33
Chris Salzberg <chrissalzberg@gmail.com>
2014-07-25 17:05:52 +0900

    Remove Spree::OrderObserver.
    It's not clear why this observer was necessary, since there is already
    an observer on payment which should run after_finalize.

--- app/models/spree/order_observer.rb
+++
@@ -1,7 +0,0 @@
-class Spree::OrderObserver < ActiveRecord::Observer
-  def after_transition(order, transition)
-    if transition.to == "complete" && order.paid?
-      order.after_finalize!
-    end
-  end
-end
--- lib/spree_license_key/engine.rb
+++ lib/spree_license_key/engine.rb
@@ -19,6 +19,6 @@ module SpreeLicenseKey
 
     config.to_prepare &method(:activate).to_proc
 
-    config.active_record.observers = ['spree/shipment_observer', 'spree/payment_observer', 'spree/order_observer']
+    config.active_record.observers = ['spree/shipment_observer', 'spree/payment_observer' ]
   end
 end
