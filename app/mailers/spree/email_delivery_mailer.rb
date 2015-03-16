5c27d5745aa9df94bcf9071df197076599e144db
Chris Salzberg <csalzberg@degica.com>
2015-03-16 14:03:06 +0900

    Send license key email asynchronously.
    We need to send emails asynchronously so that in case something,
    the mailer will keep trying to send the email.

--- app/mailers/spree/email_delivery_mailer.rb
+++ app/mailers/spree/email_delivery_mailer.rb
@@ -3,4 +3,6 @@ class Spree::EmailDeliveryMailer < ActionMailer::Base
     @inventory_units = shipment.inventory_units
     mail :to => shipment.order.user.email
   end
+
+  handle_asynchronously :electronic_delivery_email
 end
