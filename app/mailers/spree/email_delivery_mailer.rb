854be4a83d293564af75d9b77739547118182755
Chris Salzberg <csalzberg@degica.com>
2015-05-19 16:34:22 +0900

    Revert "Send license key email asynchronously."
    This reverts commit 5c27d5745aa9df94bcf9071df197076599e144db.
    
    We added this after we had issues with sendgrid that caused delayed jobs
    to fail and re-send license keys, but the commit does not actually work:
    it makes email_delivery_email asynchronous so it returns `nil`, and then
    `deliver` is called on that and fails. Luckily we override this method
    so it doesn't seem to have affected anything.
    
    Closes #17.

--- app/mailers/spree/email_delivery_mailer.rb
+++ app/mailers/spree/email_delivery_mailer.rb
@@ -3,6 +3,4 @@ class Spree::EmailDeliveryMailer < ActionMailer::Base
     @inventory_units = shipment.inventory_units
     mail :to => shipment.order.user.email
   end
-
-  handle_asynchronously :electronic_delivery_email
 end
