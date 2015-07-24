ff7c8892f4af3f8be559426a6d8d5d99cf3da25a
Christopher Locke <clocke@degica.com>
2015-07-24 12:16:21 +0900

    Add instance methods checks for the scopes

--- spec/models/spree/license_key_spec.rb
+++ spec/models/spree/license_key_spec.rb
@@ -9,12 +9,18 @@ describe Spree::LicenseKey do
   describe '.available' do
     it 'returns license keys which do not have inventory units' do
       Spree::LicenseKey.available.all.should == [ available_license_key ]
+      expect(available_license_key.available?).to eq true
+      expect(used_license_key.available?).to      eq false
+      expect(voided_license_key.available?).to    eq false
     end
   end
 
   describe '.used' do
     it 'returns license keys which have inventory units' do
       Spree::LicenseKey.used.all.should == [ used_license_key ]
+      expect(available_license_key.used?).to eq false
+      expect(used_license_key.used?).to      eq true
+      expect(voided_license_key.used?).to    eq false
     end
   end
 
