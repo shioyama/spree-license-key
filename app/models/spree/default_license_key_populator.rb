df2a0fc9eab961a187816cbacc52b9e44dfb17ba
Chris Salzberg <csalzberg@degica.com>
2015-10-08 17:29:32 +0900

    Refactor DefaultLicenseKeyPopulator class.

--- app/models/spree/default_license_key_populator.rb
+++ app/models/spree/default_license_key_populator.rb
@@ -2,7 +2,7 @@ module Spree
   class DefaultLicenseKeyPopulator < LicenseKeyPopulator
 
     def get_available_keys(inventory_unit, quantity, license_key_type=nil)
-      return false unless count_available(inventory_unit, license_key_type) >= quantity
+      return false unless count_available(license_key_type) >= quantity
       LicenseKey.available.where(
         :variant_id => inventory_unit.variant.id,
         :license_key_type_id => license_key_type.try(:id)
@@ -16,9 +16,9 @@ module Spree
 
     private
 
-    def count_available(inventory_unit, license_key_type)
+    def count_available(license_key_type)
       relation = license_key_type ? license_key_type.available : Spree::LicenseKey.available.where(license_key_type_id: nil)
-      relation.where(variant_id: inventory_unit.variant.try(:id)).count
+      relation.where(variant_id: variant.try(:id)).count
     end
   end
 end
